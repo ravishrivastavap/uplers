e91f005cc4aa890a21878c532fc5d7e13c606ddb
ravi <rpshrivastav@kabloomcorp.com>
2020-02-19 10:23:25 +0530

    project

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,65 @@
+@extends('base')
+
+@section('main')
+<div class="row">
+ <div class="col-sm-8 offset-sm-2">
+    <h1 class="display-6">Add a contact</h1>
+  <div>
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="post" action="{{ route('contacts.store') }}">
+          @csrf
+          <div class="form-group">    
+              <label for="first_name">First Name:</label>
+              <input type="text" class="form-control" name="first_name"/>
+          </div>
+
+          <div class="form-group">
+              <label for="last_name">Last Name:</label>
+              <input type="text" class="form-control" name="last_name"/>
+          </div>
+
+          <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="text" class="form-control" name="email"/>
+          </div>
+		  
+		  <div class="form-group">
+              <label for="email">Select Date:</label>
+              <input type="text" class="form-control" name="selectdate" id="selectdate"/>
+          </div>
+          <div class="form-group">
+              <label for="city">City:</label>
+              <input type="text" class="form-control" name="city"/>
+          </div>
+          <div class="form-group">
+              <label for="country">Country:</label>
+              <input type="text" class="form-control" name="country"/>
+          </div>
+          <div class="form-group">
+              <label for="job_title">Job Title:</label>
+              <input type="text" class="form-control" name="job_title"/>
+          </div>                         
+          <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+  </div>
+</div>
+</div>
+ <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<script>
+    $( function() {
+       $('#selectdate').datepicker();        
+    });
+</script>
+@endsection
